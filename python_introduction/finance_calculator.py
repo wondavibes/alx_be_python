93443f6ba74924bf2a2d1d8c9b0d48619a3fa5f8
Amos Ayemoro <81036766+wondavibes@users.noreply.github.com>
2025-09-10 21:16:46 +0100

    added dollar signs

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -6,4 +6,4 @@ monthly_savings = float(monthly_income) - float(monthly_expenses)
 Projected_savings = monthly_savings * 12 + (monthly_savings * 0.05 * 12)
 
 print("Your monthly savings are:", monthly_savings)
-print("Projected savings in one year with interest:", Projected_savings)
+print("Projected savings in one year with interest:", "$",Projected_savings)
