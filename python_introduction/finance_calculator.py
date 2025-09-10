4883f44f98d5161dc6856d9f33c7ed4a3e824985
wondavibes <amosayemoro2001@gmail.com>
2025-09-10 21:20:45 +0100

    added proper dollar signs to savings figures

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -5,5 +5,5 @@ monthly_savings = float(monthly_income) - float(monthly_expenses)
 
 Projected_savings = monthly_savings * 12 + (monthly_savings * 0.05 * 12)
 
-print("Your monthly savings are:", monthly_savings)
-print("Projected savings in one year with interest:", "$",Projected_savings)
+print(f"Your monthly savings are: ${monthly_savings:.2f}")
+print(f"Projected savings in one year with interest: ${Projected_savings:.2f}")
