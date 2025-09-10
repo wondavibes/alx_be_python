e160850c24427e8a93763cdb26445f8d1a0fde61
wondavibes <amosayemoro2001@gmail.com>
2025-09-10 21:13:39 +0100

    finance calculator for monthly and compounded savings

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,9 @@
+monthly_income = input("Enter your monthly income: ")
+monthly_expenses = input("Enter your monthly expenses: ")
+
+monthly_savings = float(monthly_income) - float(monthly_expenses)
+
+Projected_savings = monthly_savings * 12 + (monthly_savings * 0.05 * 12)
+
+print("Your monthly savings are:", monthly_savings)
+print("Projected savings in one year with interest:", Projected_savings)
