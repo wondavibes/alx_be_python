81bc4a449c38ba014e1d72bd34338419c20a4fbe
wondavibes <amosayemoro2001@gmail.com>
2025-09-17 18:02:31 +0100

    weather advice using conditionals

---
+++ control-flow/weather_advice.py
@@ -0,0 +1,9 @@
+question = input("What's the weather like today? (sunny/rainy/cold): ")
+if question == "sunny":
+    print("wear a t-shirt and sunglasses.")
+elif question == "rainy":
+    print("Don't forget your umbrella and a raincoat.")
+elif question == "cold":
+    print("Don't forget to wear a warm coat and a scarf.")
+else:
+    print("Sorry, I don't have recommendations for this weather.")
