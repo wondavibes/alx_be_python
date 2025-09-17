1524057bd2b8a06b1f6a9da95879b47e272de3d7
wondavibes <amosayemoro2001@gmail.com>
2025-09-17 22:16:30 +0100

    new variable

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,6 +1,7 @@
 
-number = int(input("Enter a number to see it's multiplication table:"))
+num = input("Enter a number to see it's multiplication table:")
 
+number = int(num)
 for i in range(1, 11):
     result = number * i
     print(f"{number} * {i} = {result}")
