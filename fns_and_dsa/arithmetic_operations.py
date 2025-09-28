d802108b52cd141d2f64dd17411868ed99a2d7c7
wondavibes <amosayemoro2001@gmail.com>
2025-09-28 17:33:58 +0100

    changed from match case to if statements

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,15 +1,14 @@
 def perform_operation(num1, num2, operation):
-    match operation:
-        case "add":
-            return num1 + num2
-        case "subtract":
-            return num1 - num2
-        case "multiply":
-            return num1 * num2
-        case "divide":
-            if num2 != 0:
-                return num1 / num2
-            else:
-                return "cannot divide by zero"
-        case _:
-            return "Invalid operation"
+    if operation == "add":
+        return num1 + num2
+    elif operation == "subtract":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        if num2 != 0:
+            return num1 / num2
+        else:
+            return "cannot divide by zero"
+    else:
+        return "Invalid operation"
