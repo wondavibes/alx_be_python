38f31bddf266c58b3d4a08f392defdcf75dc97f5
wondavibes <amosayemoro2001@gmail.com>
2025-10-05 17:38:35 +0100

    edits

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -7,7 +7,7 @@ class TestSimpleCalculator(unittest.TestCase):
     def setUp(self):
         self.calc = SimpleCalculator()
 
-    def test_add(self):
+    def test_addition(self):
         # tests the add function in simple_calculator
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, 1), 0)
