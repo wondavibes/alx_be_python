e75f699845b3143340d3b7a0c1eba9e8f4dfbd54
wondavibes <amosayemoro2001@gmail.com>
2025-10-12 20:30:45 +0100

    polymorph with shapes

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,23 @@
+import math
+
+
+class Shape:
+    def area(self):
+        raise NotImplementedError("Subclasses must implement and override this method")
+
+
+class Rectangle(Shape):
+    def __init__(self, length: float, width: float):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * self.radius**2
