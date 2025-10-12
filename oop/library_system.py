63508ae8196e5c8f5380600f936ddc5c9e7cc71c
wondavibes <amosayemoro2001@gmail.com>
2025-10-12 19:33:29 +0100

    edits

--- oop/library_system.py
+++ oop/library_system.py
@@ -4,7 +4,7 @@ class Book:
         self.author = author
 
     def __str__(self):
-        return f"{self.title} by {self.author}"
+        return f"Book: {self.title} by {self.author}"
 
 
 class EBook(Book):
